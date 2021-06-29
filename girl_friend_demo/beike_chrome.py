09bc9b083feeb197da6324318f38ccc38ecfaafe
flybetter <flybetter@163.com>
2021-06-29 15:09:25 +0800

    already  all finished

--- girl_friend_demo/beike_chrome.py
+++ girl_friend_demo/beike_chrome.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get("http://www.baidu.com")
-print(browser.page_source)
-browser.close()
+
+
+def chrome_function(url):
+    browser = webdriver.Chrome()
+    browser.get(url)
+    print(browser.page_source)
+    browser.close()
+
+
+if __name__ == '__main__':
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(2)
+    browser.get("http://nj.ke.com/ershoufang/103102654878.html?fb_expo_id=462181816624578560")
+    divs = browser.find_elements_by_xpath("//div[@class='item mytime']")
+    result = list()
+    for div in divs:
+        result.append(div.text)
+    browser.close()
--- girl_friend_demo/beike_detail.py
+++ girl_friend_demo/beike_detail.py
@@ -6,6 +6,9 @@ import json
 from pandas.io.json import json_normalize
 import re
 import pandas as pd
+from selenium import webdriver
+import datetime
+
 
 def read_csv():
     df = pd.read_csv("demo.csv")
@@ -13,19 +16,32 @@ def read_csv():
 
 
 def url_action(url):
-    url_http = url.replace('https', 'http')
-    print(url_http)
-    response = urllib.request.urlopen(url_http)
-    print(response.read().decode('utf-8'))
+    print(url)
+    browser = webdriver.Chrome()
+    browser.get(url)
+    divs = browser.find_elements_by_xpath("//div[@class='item mytime']")
+    result_demo = []
+    for div in divs:
+        result_demo.append(div.text)
+    browser.close()
+    return result_demo
+
+
+def save_csv(df, results):
+    s = pd.Series(results)
+    df_times = pd.DataFrame(s)
+    date=datetime.datetime.strftime('%Y-%m-%d')
+    df_times.columns = ['detail_times'+date]
+    df_times['times'+date] = df_times['detail_times'+date].map(lambda x: len(x))
+    df_final = pd.concat([df, df_times], axis=1)
+    df_final.to_csv("demo2.csv")
 
 
 if __name__ == '__main__':
-    # list = read_csv()
-    # for url in list:
-    #     url_action(url)
-
-    url = "http://nj.ke.com/ershoufang/103102654878.html?fb_expo_id=462181816624578560"
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(url=request)
-    print(response.read().decode('utf-8'))
-
+    results = list()
+    df = pd.read_csv("demo.csv")
+    list = df['actionUrl'].to_list()
+    for i, url in enumerate(list):
+        print(i)
+        results.append(url_action(url))
+    save_csv(df, results)
