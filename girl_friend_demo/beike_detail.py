4da27bccd00f958ec481d139a5f4f531818ab3ff
flybetter <flybetter@163.com>
2021-06-30 09:00:45 +0800

    add favourite stars.

--- girl_friend_demo/beike_detail.py
+++ girl_friend_demo/beike_detail.py
@@ -23,25 +23,40 @@ def url_action(url):
     result_demo = []
     for div in divs:
         result_demo.append(div.text)
+
+    count = browser.find_elements_by_xpath("//span[@class='count']")[0].text
     browser.close()
-    return result_demo
+    return result_demo, count
 
 
-def save_csv(df, results):
+def save_csv(df, results, stars_list):
     s = pd.Series(results)
     df_times = pd.DataFrame(s)
-    date=datetime.datetime.strftime('%Y-%m-%d')
-    df_times.columns = ['detail_times'+date]
-    df_times['times'+date] = df_times['detail_times'+date].map(lambda x: len(x))
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    df_times.columns = ['detail_times_' + date]
+    df_times['times' + date] = df_times['detail_times_' + date].map(lambda x: len(x))
     df_final = pd.concat([df, df_times], axis=1)
+
+    s_1 = pd.Series(stars_list)
+    df_stars = pd.DataFrame(s_1)
+    df_stars.columns = ['starts_' + date]
+    df_final = pd.concat([df_final, df_stars], axis=1)
+
     df_final.to_csv("demo2.csv")
 
 
 if __name__ == '__main__':
     results = list()
+    stars_list = list()
     df = pd.read_csv("demo.csv")
     list = df['actionUrl'].to_list()
     for i, url in enumerate(list):
         print(i)
-        results.append(url_action(url))
-    save_csv(df, results)
+        result_list, start_count = url_action(url)
+        results.append(result_list)
+        stars_list.append(start_count)
+
+    save_csv(df, results, stars_list)
+
+
+
